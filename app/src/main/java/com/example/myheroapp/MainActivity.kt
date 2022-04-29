d80b08bcd0e5ecbdcd867262be367e543d39c00b
JorgeCorrea <61559219+edgar-cor@users.noreply.github.com>
2022-04-29 01:26:26 -0500

    add feature to take phothos

--- app/src/main/java/com/example/myheroapp/MainActivity.kt
+++ app/src/main/java/com/example/myheroapp/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.myheroapp
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.ImageView
+import androidx.core.graphics.drawable.toBitmap
 import com.example.myheroapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var heroImage: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
@@ -20,11 +26,32 @@ class MainActivity : AppCompatActivity() {
             val hero = Superhero( superHeroName, alterEgo, bio, power )
             openDetailActivity(hero)
         }
+        heroImage = binding.heroImage
+        binding.heroImage.setOnClickListener{
+            openCamara()
+        }
+
+    }
+
+    private fun openCamara() {
+       val cameraIntent = Intent( MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(cameraIntent, 1000)
+
     }
 
     private fun openDetailActivity(superhero: Superhero) {
         val intent = Intent(this, DetailActivity::class.java)
         intent.putExtra(DetailActivity.SUPERHERO_KEY, superhero)
+        intent.putExtra(DetailActivity.BITMAP_KEY, heroImage.drawable.toBitmap())
         startActivity(intent)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == 1000){
+            val extras = data?.extras
+            val heroBitmap = extras?.getParcelable<Bitmap>("data")
+            heroImage.setImageBitmap(heroBitmap)
+        }
     }
 }
